91b6779eb8ff7c8d14e6915d0a7002d4ccfb6d2c
LucasNomme <lucas.loulou232023@gmail.com>
2025-07-14 09:54:09 +0200

    Create layout.jsx

---
+++ app/layout.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'LucasIAdu13',
+  description: 'Le chatbot hackeur stylé de Lucas',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="fr">
+      <body>{children}</body>
+    </html>
+  )
+}
